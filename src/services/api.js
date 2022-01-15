b50200e72f5665977ce4cf441837e3b3a72fd6e7
Swee <sweechong.wong@gmail.com>
2022-01-15 03:04:07 +0200

    fix api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "/modules",
+  baseURL: "",
   headers: {
     "Content-Type": "application/json",
   },
--- src/services/data.services.js
+++ src/services/data.services.js
@@ -1,41 +1,41 @@
 import api from './api';
 
 export const getQuotes = () => {
-  return api.get('/quotes/');
+  return api.get('/modules/quotes/');
 }
 
 export const getQuotesConfig = () => {
-  return api.get('/quotesConfig/');
+  return api.get('/modules/quotesConfig/');
 }
 
 export const saveQuotesConfig = (payload) => {
-  return api.post('/quotesConfig/', payload)
+  return api.post('/modules/quotesConfig/', payload)
 }
 
 export const patchQuotesConfig = (qcid, payload) => {
-  return api.patch(`/quotesConfig/${qcid}/`, payload);
+  return api.patch(`/modules/quotesConfig/${qcid}/`, payload);
 }
 
 export const getQuotesTags = () => {
-  return api.get('/quotesTags/');
+  return api.get('/modules/quotesTags/');
 }
 
 export const saveQuotesTags = (payload) => {
-  return api.post('/quotesTags/', payload)
+  return api.post('/modules/quotesTags/', payload)
 }
 
 export const patchQuotesTags = (qcid, payload) => {
-  return api.patch(`/quotesTags/${qcid}/`, payload);
+  return api.patch(`/modules/quotesTags/${qcid}/`, payload);
 }
 
 export const getSingleQuote = (qid) => {
-  return api.get(`/quotes/${qid}/`)
+  return api.get(`/modules/quotes/${qid}/`)
 }
 
 export const deleteSingleQuote = (qid) => {
-  return api.delete(`/quotes/${qid}/`)
+  return api.delete(`/modules/quotes/${qid}/`)
 }
 
 export const saveSingleQuote = (qid, payload) => {
-  return api.put(`/quotes/${qid}/`, payload)
+  return api.put(`/modules/quotes/${qid}/`, payload)
 }
